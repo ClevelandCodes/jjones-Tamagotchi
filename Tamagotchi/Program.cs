21a052885fc012798196dacea9f255fd647a9ae3
Jamayka Jones <jamayka.jones@gmail.com>
2016-04-06 14:40:40 -0400

    first commit

---
+++ Tamagotchi/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tamagotchi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            tamagotchi t1 = new tamagotchi("Luna", "M", 100, 100);
+            Console.WriteLine("Name: {0}, Gender: {1}, Fullness: {2}, Happiness: {3}", t1.getName(), t1.getGender(), t1.getFullness(), t1.getHappiness());
+            //Console.ReadLine();
+            while (t1.getHappiness() > 0 && t1.getFullness() > 0)
+            {
+                Console.WriteLine("Would you like to pet or feed {0}?", t1.getName() );
+                string input = Console.ReadLine();
+
+                if (input == "pet") {
+                    t1.pet();
+                }
+                else if (input == "feed") {
+                    t1.feed();
+                }
+                else {
+                    Console.WriteLine("Please only use 'pet' or 'feed'.");
+
+                }
+                t1.dostuff();
+                Console.WriteLine("Name: {0}, Gender: {1}, Fullness: {2}, Happiness: {3}", t1.getName(), t1.getGender(), t1.getFullness(), t1.getHappiness());
+            }
+        }
+    }
+}
---
+++ Tamagotchi/tamagotchi.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tamagotchi
+{
+    class tamagotchi
+    {
+        private string Name;
+        private string Gender;
+        private int Fullness;
+        private int Happiness;
+
+
+        public tamagotchi(string name, string gender, int fullness, int happiness)
+        {
+            Name = name;
+            Gender = gender;
+            Fullness = fullness;
+            Happiness = happiness;
+        }
+
+        public string getName()
+        {
+            return Name;
+        }
+
+        public string getGender()
+        {
+            return Gender;
+        }
+
+        public int getFullness()
+        {
+            return Fullness;
+        }
+
+        public int getHappiness()
+        {
+            return Happiness;
+        }
+
+        public void pet()
+        {
+            Random rnd = new Random();
+            int CoolNumberH = rnd.Next(10); //chooses a random number from 0-10 and stores it in the new varible
+            Console.WriteLine(CoolNumberH); //tells user the random number it chose
+            Happiness += CoolNumberH; //increments the happiness by the random number it chose
+        }
+
+        public void feed()
+        {
+            Random rnd = new Random();
+            int CoolNumberF = rnd.Next(10);
+            Console.WriteLine(CoolNumberF);
+            Fullness += CoolNumberF;
+        }
+
+        public void dostuff()
+        {
+            Random rnd = new Random();
+            Fullness -= rnd.Next(5);
+            Happiness -= rnd.Next(5);
+        }
+    }
+}
